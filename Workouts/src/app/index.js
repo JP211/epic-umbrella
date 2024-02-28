9f9cca1bfd0f69fbdcd1d36434bf95e2437b36bd
Jessica Cockerell <jlpeters211@gmail.com>
2024-02-28 15:44:47 -0600

    Set up navigation

--- Workouts/src/app/index.js
+++ Workouts/src/app/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View, FlatList } from "react-native";
-import exercises from "./assets/data/exercises.json";
-import ExerciseListItem from "./src/components/ExerciseListItem/ExcerciseListItem";
+import exercises from "../../assets/data/exercises.json";
+import ExerciseListItem from "../../src/components/ExerciseListItem/ExcerciseListItem";
 
 export default function App() {
   return (
@@ -18,9 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "gainsboro",
+    backgroundColor: "ghostwhite",
     justifyContent: "center",
     padding: 10,
-    paddingTop: 70,
   },
 });
--- Workouts/src/components/ExerciseListItem/ExcerciseListItem.jsx
+++ Workouts/src/components/ExerciseListItem/ExcerciseListItem.jsx
@@ -1,39 +1,44 @@
 import { StyleSheet, Text, View } from "react-native";
 
-
 export default function ExerciseListItem({ item }) {
   return (
     <View style={styles.exerciseContainer}>
       <Text style={styles.exerciseName}>{item.name}</Text>
       <Text style={styles.muscleName}>
-        <Text style={styles.subTitle}>{item.muscle}</Text> | <Text style={styles.subTitle}>{item.equipment}</Text>
+        <Text style={styles.subTitle}>{item.muscle}</Text> |{" "}
+        <Text style={styles.subTitle}>{item.equipment}</Text>
       </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "gainsboro",
-      justifyContent: "center",
-      padding: 10,
+  exerciseContainer: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+    marginHorizontal: 2,
+    
+    // shadow settings
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    exerciseContainer: {
-      backgroundColor: "#fff",
-      padding: 10,
-      borderRadius: 10,
-      gap: 5,
-    },
-    exerciseName: {
-      fontSize: 20,
-      fontWeight: "500",
-    },
-    muscleName: {
-      color: "dimgray",
-    },
-    subTitle: {
-        textTransform: 'capitalize',
-    },
-  });
-  
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  muscleName: {
+    color: "dimgray",
+  },
+  subTitle: {
+    textTransform: "capitalize",
+  },
+});
