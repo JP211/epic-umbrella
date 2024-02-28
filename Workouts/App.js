2bc0b0bd0090771f3b102c504f04df012e028d7c
Jessica Cockerell <jlpeters211@gmail.com>
2024-02-28 15:06:09 -0600

    Create Exercise List component

--- Workouts/App.js
+++ Workouts/App.js
@@ -1,19 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import exercises from "./assets/data/exercises.json";
+import ExerciseListItem from "./src/components/ExerciseListItem/ExcerciseListItem";
 
 export default function App() {
-  const exercise = exercises[0];
-  console.log(exercise);
-
   return (
     <View style={styles.container}>
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>{exercise.name}</Text>
-        <Text style={styles.muscleName}>
-          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
-        </Text>
-      </View>
+      <FlatList
+        data={exercises}
+        contentContainerStyle={{ gap: 5 }}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => <ExerciseListItem item={item} />}
+      />
     </View>
   );
 }
@@ -24,18 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: "gainsboro",
     justifyContent: "center",
     padding: 10,
-  },
-  exerciseContainer: {
-    backgroundColor: "#fff",
-    padding: 10,
-    borderRadius: 10,
-    gap: 5,
-  },
-  exerciseName: {
-    fontSize: 20,
-    fontWeight: "500",
-  },
-  muscleName: {
-    color: "dimgray",
+    paddingTop: 70,
   },
 });
